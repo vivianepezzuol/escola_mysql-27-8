173e0f8d1055f615ff8cd10e2049c789d69591d6
viviane Pezzuol <vivianepezzuol@gmail.com>
2025-08-28 21:42:42 -0300

    java 28-08

---
+++ src/main/java/com/senai/escola/Controller/ProfessorController.java
@@ -0,0 +1,43 @@
+package com.senai.escola.Controller;
+
+import com.senai.escola.Models.Aluno;
+import com.senai.escola.Service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public List<Aluno> buscarAlunos(){
+        return alunoService.buscarTodosAlunos();
+    }
+
+    @PostMapping
+    public Aluno salvar(@RequestBody Aluno aluno){
+        return alunoService.salvarNovoAluno(aluno);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void excluirAluno( @PathVariable Long id){
+        alunoService.deletarAluno(id);
+    }
+
+
+    @GetMapping("/{id}")
+    public Aluno buscaAlunoPorId(@PathVariable Long id){
+        return alunoService.buscarAlunoId(id);
+    }
+
+
+
+}
